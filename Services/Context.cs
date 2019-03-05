bcfeb5bbd097d5c4687779edf7695b774911546f
vova-lantsov-dev <vova.lantsov.25.private@gmail.com>
2019-03-05 20:15:05 +0200

    FEATURE reviews collection in db context

--- Services/Context.cs
+++ Services/Context.cs
@@ -1,15 +1,20 @@
 using Microsoft.Extensions.Options;
 using MongoDB.Driver;
+using ReviewGrabberBot.Models;
 using ReviewGrabberBot.Options;
 
 namespace ReviewGrabberBot.Services
 {
     public sealed class Context
     {
+        public readonly IMongoCollection<Review> Reviews;
+        
         public Context(IOptions<ContextOptions> options)
         {
             var mongoClient = new MongoClient(options.Value.ConnectionString);
             var db = mongoClient.GetDatabase(options.Value.DatabaseName);
+
+            Reviews = db.GetCollection<Review>(options.Value.ReviewCollectionName);
         }
     }
 }
