6c01fa575ce66285679b0b6225a69986e7c85cff
vova-lantsov-dev <vova.lantsov.25.private@gmail.com>
2019-03-05 19:05:21 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace ReviewGrabberBot
+{
+    public class Program
+    {
+        internal static async Task Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
