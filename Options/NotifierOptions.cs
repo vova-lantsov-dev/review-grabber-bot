5f2472fcd596cb38d598f999c1a84885eb6044cd
vova-lantsov-dev <vova.lantsov.25.private@gmail.com>
2019-03-07 16:11:27 +0200

    FEATURE notifier options

---
+++ Options/NotifierOptions.cs
@@ -0,0 +1,10 @@
+using System.Collections.Generic;
+using ReviewGrabberBot.Models;
+
+namespace ReviewGrabberBot.Options
+{
+    public sealed class NotifierOptions
+    {
+        public List<Restaurant> Restaurants;
+    }
+}
