6c2a8e666a1252b396ac12ee5688a4837fa1093d
vova-lantsov-dev <vova.lantsov.25.private@gmail.com>
2019-03-08 22:06:54 +0200

    REFACTORING options configuring moved to separate extension method

---
+++ Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,49 @@
+using System.Collections.Generic;
+using System.IO;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json;
+using ReviewGrabberBot.Exceptions;
+using ReviewGrabberBot.Models;
+using ReviewGrabberBot.Options;
+
+namespace ReviewGrabberBot.Extensions
+{
+    internal static class ServiceCollectionExtensions
+    {
+        internal static void ConfigureOptions(this IServiceCollection services, IConfiguration config)
+        {
+            services.Configure<ContextOptions>(options =>
+            {
+                options.ConnectionString = config["CONNECTION_STRING"] ?? "mongodb://localhost";
+                options.DatabaseName = "reviewbot";
+                options.ReviewCollectionName = "reviews";
+            });
+            services.Configure<BotOptions>(options =>
+            {
+                options.AdminId = config["ADMIN_ID"]
+                                  ?? throw new EnvironmentVariableNotFoundException("ADMIN_ID");
+            });
+            services.Configure<NotifierOptions>(options =>
+            {
+                options.WorkingDirectory = config["WORKING_DIRECTORY"]
+                                           ?? throw new EnvironmentVariableNotFoundException("WORKING_DIRECTORY");
+                if (!Directory.Exists(options.WorkingDirectory))
+                    throw new EnvironmentVariableDirectoryNotFoundException("WORKING_DIRECTORY");
+
+                options.ScrapyPath = config["SCRAPY_PATH"]
+                                     ?? throw new EnvironmentVariableNotFoundException("SCRAPY_PATH");
+                if (!File.Exists(options.ScrapyPath))
+                    throw new EnvironmentVariableFileNotFoundException("SCRAPY_PATH");
+                        
+                var notifierOptionsPath = config["NOTIFIER_OPTIONS_PATH"]
+                                          ?? throw new EnvironmentVariableNotFoundException("NOTIFIER_OPTIONS_PATH");
+                if (!File.Exists(notifierOptionsPath))
+                    throw new EnvironmentVariableFileNotFoundException("NOTIFIER_OPTIONS_PATH");
+                        
+                var json = File.ReadAllText(notifierOptionsPath);
+                options.Restaurants = JsonConvert.DeserializeObject<List<Restaurant>>(json);
+            });
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,13 +1,9 @@
-﻿using System.Collections.Generic;
-using System.IO;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Newtonsoft.Json;
-using ReviewGrabberBot.Exceptions;
+using ReviewGrabberBot.Extensions;
 using ReviewGrabberBot.Handlers;
-using ReviewGrabberBot.Models;
 using ReviewGrabberBot.Options;
 using ReviewGrabberBot.Services;
 using Telegram.Bot;
@@ -25,38 +21,7 @@ namespace ReviewGrabberBot
                 })
                 .ConfigureServices((context, services) =>
                 {
-                    var config = context.Configuration;
-                    services.Configure<ContextOptions>(options =>
-                    {
-                        options.ConnectionString = config["CONNECTION_STRING"] ?? "mongodb://localhost";
-                        options.DatabaseName = "reviewbot";
-                        options.ReviewCollectionName = "reviews";
-                    });
-                    services.Configure<BotOptions>(options =>
-                    {
-                        options.AdminId = config["ADMIN_ID"]
-                            ?? throw new EnvironmentVariableNotFoundException("ADMIN_ID");
-                    });
-                    services.Configure<NotifierOptions>(options =>
-                    {
-                        options.WorkingDirectory = config["WORKING_DIRECTORY"]
-                            ?? throw new EnvironmentVariableNotFoundException("WORKING_DIRECTORY");
-                        if (!Directory.Exists(options.WorkingDirectory))
-                            throw new EnvironmentVariableDirectoryNotFoundException("WORKING_DIRECTORY");
-
-                        options.ScrapyPath = config["SCRAPY_PATH"]
-                            ?? throw new EnvironmentVariableNotFoundException("SCRAPY_PATH");
-                        if (!File.Exists(options.ScrapyPath))
-                            throw new EnvironmentVariableFileNotFoundException("SCRAPY_PATH");
-                        
-                        var notifierOptionsPath = config["NOTIFIER_OPTIONS_PATH"]
-                            ?? throw new EnvironmentVariableNotFoundException("NOTIFIER_OPTIONS_PATH");
-                        if (!File.Exists(notifierOptionsPath))
-                            throw new EnvironmentVariableFileNotFoundException("NOTIFIER_OPTIONS_PATH");
-                        
-                        var json = File.ReadAllText(notifierOptionsPath);
-                        options.Restaurants = JsonConvert.DeserializeObject<List<Restaurant>>(json);
-                    });
+                    services.ConfigureOptions(context.Configuration);
 
                     services.AddSingleton<Context>();
                     services.AddSingleton(new TelegramBotClient(Constants.BotToken));
